66d90fcfb8807c3534d8872e762daea252f2b17c
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-22 23:57:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,18 +4,20 @@ $(document).ready(function(){
 
 // call functions here
 function getIt() {
-  $("p").on("click", (e) => window.alert("Hey!"))
+  $("p").on("click", (e) => window.alert("Hey!"));
 }
 
+getIt();
+
 function frameIt() {
-  
+
 }
 
 function pressIt() {
-  
+
 }
 
 function submitIt() {
-  
+
 }
 });
