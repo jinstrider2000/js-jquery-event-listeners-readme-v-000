4ecbc5a70224d30f871e1132353f093b9a31a703
Efrain Perez Jr <efrainperezjr@live.com>
2018-03-23 00:43:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,7 +4,7 @@ function getIt() {
 }
 
 function frameIt() {
-  $("img").on("click", (e) => $(this).attr("class", "tasty"));
+  $("img").on("click", (e) => $("img").attr("class", "tasty"));
   // $("img").attr("class", "tasty");
 }
 
